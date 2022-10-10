6736f77eaa3f4f02dbf025bf13c60d719f060285
1mshy <imshi1@protonmail.com>
2022-10-10 12:14:03 -0400

    doing update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,6 +1,30 @@
 exports.run = async (client, message, args) => {
+    const execSync = require('child_process').execSync;
+    // import { execSync } from 'child_process';  // replace ^ if using ES modules
+    
+    const updateInfo = execSync('git remote update', { encoding: 'utf-8' });  // the default is 'buffer'
+    const status = execSync('git status -uno', { encoding: 'utf-8' });
+    let stats = status.split("\n")[1].split(" ")[3];
+    // up: up to date
+    if(stats=='up') return;
+    // behind: needs to update
+    const merge = execSync('git pull origin master', { encoding: 'utf-8' });
+    // getting the files that were updated
+    let arr = merge.split("\n");
+    let index = arr.indexOf("Fast-forward") + 1
+    let isCommand = true;
+    let reload = require("../commands/reload.js")
+    while(isCommand)
+    {
+        // gets the name of the command
+        //Ex. src/commands/update.js | 7++++++ -> update.js
+        let commandName = arr[index].split("/")[2].split("|")[0].trim().split(".")[0];
+        await reload.run(client, message, commandName)
 
-    await message.channel.send("didn't work!").catch(console.error);
+        index++;
+        isCommand = arr[index].split("/")[0]=="src";
+    }
+    await message.reply("Update completed.").catch(console.error);
 }
 
 exports.name = "update";
