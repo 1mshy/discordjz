12f607183ce330f44fbb28aba8e7977a039e8469
1mshy <imshi1@protonmail.com>
2022-10-10 10:14:40 -0400

    Less flagging for wrong commands

--- src/errors/commandNotFound.js
+++ src/errors/commandNotFound.js
@@ -1,6 +1,8 @@
 exports.run = async (client, message, args) => {
+    // when users input something like ..., it won't flag it as a wrong command
+    if(message.content[1]=='.') return;
     await message.channel.send("Command not found").catch(console.error);
 }
 
 exports.name = "commandNotFound";
-exports.description = "Default description";
+exports.description = "Run when user requested command does not exist";
