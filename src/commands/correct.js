b9266eeb2eca1619a98e1fc03894898acaf792e6
1mshy <imshi1@protonmail.com>
2022-10-10 10:32:15 -0400

    Added command to fix spelling mistakes

--- src/commands/correct.js
+++ src/commands/correct.js
@@ -1,9 +1,9 @@
 exports.run = async (client, message, ...prompt) => {
     prom = prompt.join(" ")
     const ai = require("../extra/aiResponse.js");
-    let res = await ai.run(prom);
+    let res = await ai.edit(prom);
     await message.reply(res);
 }
 
-exports.name = "ai";
-exports.description = ".ai {phrase}";
+exports.name = "correct";
+exports.description = ".correct {phrase}";
--- src/extra/aiResponse.js
+++ src/extra/aiResponse.js
@@ -1,7 +1,8 @@
+const config = require("../config.json");
+
 // takes in client to use the key
 exports.run = async (prompt) => {
     const { Configuration, OpenAIApi } = require("openai");
-    const config = require("../config.json")
     const configuration = new Configuration({
     apiKey: config.openaikey,
     });
@@ -16,6 +17,27 @@ exports.run = async (prompt) => {
     best_of: 1,
     frequency_penalty: 1,
     presence_penalty: 0.2,
+    });
+
+    // response is found in data.choices[0].text
+    res = response.data.choices[0].text;
+
+    if(res.length > 1999)
+        res = res.slice(0,1999)
+    return res;
+}
+// takes in client to use the key
+exports.edit = async (prompt) => {
+    const { Configuration, OpenAIApi } = require("openai");
+    const configuration = new Configuration({
+    apiKey: config.openaikey,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createEdit({
+    model: "text-davinci-edit-001",
+    input: prompt,
+    instruction: "Fix the spelling mistakes",
+    top_p: 0.7,
     });
 
     // response is found in data.choices[0].text
